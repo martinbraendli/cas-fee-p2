31a707846974feea34ca0f8ee334c0c9fff808b5
gitfilix <runlevel07@gmail.com>
2015-07-22 14:34:56 +0200

    Added temporary Templates

---
+++ tempTemplates/js/fb-controller.js
@@ -0,0 +1,8 @@
+/**
+ * Created by FLX on 20.07.2015.
+ */
+var searchOptions = angular.module('fb-app', [])
+
+.controller('showController', function($scope){
+    $scope.showSearchOptions = false;
+});
