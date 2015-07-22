5e2a64fc01cd11aae1829aa1c10143c7c48764da
Martin Brändli <martin.braendli@zkb.ch>
2015-07-22 20:34:42 +0200

    initial server setup added required client libs into public/js/lib gulp setup for copy to build-folder

--- Server/index.ts
+++ Server/index.ts
@@ -1,6 +1,19 @@
 /**
  * Entry-Point for server
  */
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require("express-session");
+var app = express();
+
+var router = express.Router();
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 
-// server siehe abend 7 --> express
+// static serving
+app.use(express.static(__dirname + '/public'));
+
+http.createServer(app).listen(3000);
--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var tsd = require('gulp-tsd');
+var ts = require('gulp-typescript');
 
-/**
- * sass function: style sass
- */
-gulp.task('sass', function(){
-    // read all scss files
-    gulp.src('scss/*.scss')
-        // load sass
-        .pipe(sass())
-        // prevent gulp from crashing due tu scss files
-        .on('error', function (error){
-            console.error("there was an scss syntax error -hey check your scss-file");
-            this.emit('end');
-        })
-        // extend autoprefixer for the last 3 browser versions
-        .pipe(autoprefixer("last 3 version", "safari 5", "ie 9"))
-        // pipe it to css folder
-        .pipe(gulp.dest('Server/public/css/'));
-    console.log('new css written');
-});
+gulp.task('default', []);
+
+gulp.task('build', ['sass', 'compileTypescript', 'copyToBuild']);
 
 /**
  * TSD Task "reinstall"
@@ -33,4 +18,55 @@ gulp.task('tsd', function (callback) {
     }, callback);
 });
 
-gulp.task('default', []);
+/**
+ * Fetch needed client libs from node_modules into public part of application
+ */
+gulp.task('copyLibs', function () {
+    gulp.src(['node_modules/angular/angular.js', 'node_modules/angular-route/angular-route.js'])
+        .pipe(gulp.dest('Server/public/js/lib'));
+});
+
+
+/**
+ * sass function: style sass
+ */
+gulp.task('sass', function () {
+    // read all scss files
+    gulp.src('scss/*.scss')
+        // load sass
+        .pipe(sass())
+        // prevent gulp from crashing due tu scss files
+        .on('error', function (error) {
+            console.error("there was an scss syntax error -hey check your scss-file");
+            this.emit('end');
+        })
+        // extend autoprefixer for the last 3 browser versions
+        .pipe(autoprefixer("last 3 version", "safari 5", "ie 9"))
+        // pipe it to css folder
+        .pipe(gulp.dest('Server/public/css/'));
+    console.log('new css written');
+});
+
+/**
+ * compile typescript
+ */
+gulp.task('compileTypescript', function () {
+    var tsResult = gulp.src('Server/**/*.ts')
+        .pipe(ts({
+            noImplicitAny: true,
+            outDir: 'output'
+        }));
+    tsResult.js.pipe(gulp.dest('build'));
+});
+
+/**
+ * Copy all files to build
+ */
+gulp.task('copyToBuild', function () {
+    gulp.src(['Server/**/*.js',
+        'Server/**/*.json',
+        'Server/**/*.css',
+        'Server/**/*.html'])
+        .pipe(gulp.dest('build'));
+});
+
