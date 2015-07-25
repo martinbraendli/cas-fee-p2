62b76e598885044d10a9af2497f0b5be013aee37
Martin Brändli <martin.braendli@zkb.ch>
2015-07-25 14:15:34 +0200

    database access for user and recipe

--- Server/services/recipeStore.ts
+++ Server/services/recipeStore.ts
@@ -1,1 +1,36 @@
-///<reference path='../../typings/tsd.d.ts' />
+///<reference path='../_reference.ts' />
+
+/**
+ * recipeStore
+ */
+
+var Datastore = require('nedb');
+var db = new Datastore({filename: './data/recipe.db', autoload: true});
+
+/**
+ * load all available recipes
+ * @param callback
+ */
+function publicLoadAll(callback) {
+    console.log("recipeStore - loadAll()");
+    db.find({}, function (err, recipes) {
+        callback(err, recipes);
+    });
+}
+
+/**
+ * load one recipe by it's id
+ * @param recipeId
+ * @param callback
+ */
+function publicLoadRecipe(recipeId:string, callback) {
+    console.log("recipeStore - loadRecipe('" + recipeId + "')");
+    db.findOne({_id: recipeId}, function (err, recipe) {
+        callback(err, recipe);
+    });
+}
+
+module.exports = {
+    loadAll: publicLoadAll,
+    loadRecipe: publicLoadRecipe
+};
--- Server/services/userStore.ts
+++ Server/services/userStore.ts
@@ -1,1 +1,35 @@
-///<reference path='../../typings/tsd.d.ts' />
+///<reference path='../_reference.ts' />
+
+/**
+ * userStore
+ */
+
+/**
+ * load one user by its id
+ * @param userId
+ * @param callback
+ */
+function publicLoadUser(userId:string, callback) {
+    console.log("userStore - loadUser('" + userId + "')");
+
+
+    //db.findOne({_id: id}, function (err, doc) {
+    //    callback(err, doc);
+    //});
+
+    // TODO read user from database
+
+    var users = [];
+    users[0] = {
+        "_id": 2,
+        "name": "rowens1",
+        "email": "ntorres1@weather.com",
+        "password": "rowens1"
+    };
+
+    callback({}, users);
+}
+
+module.exports = {
+    loadUser: publicLoadUser
+};
