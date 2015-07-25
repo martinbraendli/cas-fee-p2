98471ee864f7493fb76867ddbfc1e005eaf39830
Martin Brändli <martin.braendli@zkb.ch>
2015-07-25 21:53:21 +0200

    new route for load all ratings by recipe

---
+++ Server/routes/ratingRoutes.ts
@@ -0,0 +1,21 @@
+///<reference path='../_reference.ts' />
+
+/**
+ * recipeRoutes
+ */
+
+var ratingCtrl = require('../controller/ratingController.js');
+
+/**
+ * Recipe routes
+ * @type {Router}
+ */
+module.exports = (function () {
+    var express = require('express');
+
+    var router = express.Router();
+
+    router.get("/:recipeId/", ratingCtrl.getRatings);
+
+    return router;
+})();
