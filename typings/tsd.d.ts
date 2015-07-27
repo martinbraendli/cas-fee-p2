2c71fca5ea15ac6bc1bbc44bab590a2c13dd5f4f
Martin Brändli <martin.braendli@zkb.ch>
2015-07-27 12:39:37 +0200

    added cookie-parser

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,6 +1,7 @@
 /// <reference path="angularjs/angular-route.d.ts" />
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="body-parser/body-parser.d.ts" />
+/// <reference path="cookie-parser/cookie-parser.d.ts" />
 /// <reference path="express-session/express-session.d.ts" />
 /// <reference path="express/express.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
