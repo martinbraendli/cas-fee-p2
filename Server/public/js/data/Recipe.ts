4f2d11364f43fee9ff05056df4391befb37b0092
Martin Brändli <martin.braendli@zkb.ch>
2015-07-27 21:39:02 +0200

    readded ingredients and recipes

---
+++ Server/public/js/data/Recipe.ts
@@ -0,0 +1,19 @@
+///<reference path='../_reference.ts' />
+module fettyBossy.Data {
+    'use strict';
+
+    export interface IRecipe {
+        _id: string;
+        userId: string;
+
+        title: string; // name of the recipe
+        numPerson: number; // number of person
+        bakingTime: number; // in minutes
+        preparationTime: number; // in minutes
+
+        //images: Array<fettyBossy.Data.IImage>;
+        //ingredients: Array<fettyBossy.Data.IIngredient>;
+        //preparationSteps: Array<fettyBossy.Data.IPreparationStep>;
+        //ratings: Array<fettyBossy.Data.IRating>;
+    }
+}
