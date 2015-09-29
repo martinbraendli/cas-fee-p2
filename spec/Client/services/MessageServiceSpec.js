c1f6e4b23ca42fe179f099817e2eb8ee35e0198e
Martin Brändli <martin.braendli@zkb.ch>
2015-09-29 21:47:33 +0200

    Initial unit test setup

---
+++ spec/Client/services/MessageServiceSpec.js
@@ -0,0 +1,72 @@
+/**
+ * Jasmine spec's für NotePro
+ */
+describe("FettyBossy - Message Service", function () {
+    var service;
+    var originalTimeout;
+
+    beforeEach(module("fettyBossy"));
+
+    beforeEach(inject(function (MessageService) {
+        service = MessageService;
+    }));
+
+    beforeEach(function () {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 500; // 500ms
+    });
+
+    afterEach(function () {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
+
+    it('should set message', function () {
+        expect(service).toBeDefined();
+
+        service.setMessage("Testmessage", 0);
+
+        var message = service.getMessage();
+        expect(message).toBeDefined();
+        expect(message.text).toBe("Testmessage");
+        expect(message.severity).toBe(0);
+    });
+
+    it('should replace message', function () {
+        expect(service).toBeDefined();
+
+        service.setMessage("Testmessage1", 0);
+        service.setMessage("Testmessage2", 1);
+        service.setMessage("Testmessage3", 2);
+
+        var message = service.getMessage();
+        expect(message).toBeDefined();
+        expect(message.text).toBe("Testmessage3");
+        expect(message.severity).toBe(2);
+    });
+
+    it('should reset message', function () {
+        expect(service).toBeDefined();
+
+        service.setMessage("Testmessage1", 0);
+        var message = service.getMessage();
+        expect(message).toBeDefined();
+        expect(message.text).toBe("Testmessage1");
+        expect(message.severity).toBe(0);
+
+        service.setMessage(null, null);
+        message = service.getMessage();
+        expect(message).toBeDefined();
+        expect(message.text).toBeFalsy();
+        expect(message.severity).toBeFalsy();
+    });
+
+    it("should call listener", function (done) {
+        expect(service).toBeDefined();
+
+        service.addListener(done);
+
+        service.setMessage("Testmessage1", 0);
+        message = service.getMessage();
+        expect(message).toBeDefined();
+    });
+});
