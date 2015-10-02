ae3f4b8bef2a07205151b3288ceb27762b72d6ae
Martin Brändli <martin.braendli@zkb.ch>
2015-10-02 21:19:06 +0200

    please use git history to find old states.

--- tempTemplates/js/fb-controller.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by FLX on 20.07.2015.
- */
-var searchOptions = angular.module('fb-app', [])
-
-.controller('showController', function($scope){
-    $scope.showSearchOptions = false;
-});
