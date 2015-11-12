dc5c9b935719b2f85aefcc372aa600edd1841083
Martin Brändli <martin.braendli@zkb.ch>
2015-11-12 17:45:10 +0100

    Testsetup for Repository

--- spec/Client/services/RepositoryServiceSpec.js
+++ spec/Client/services/RepositoryServiceSpec.js
@@ -3,19 +3,49 @@
  */
 describe("FettyBossy - Repository Service", function () {
     var service;
+    var httpBackend;
 
     beforeEach(module("fettyBossy"));
 
-    beforeEach(inject(function (RepositoryService) {
-        service = RepositoryService;
+    beforeEach(inject(function ($injector, $httpBackend) {
+        service = $injector.get('RepositoryService');
+        httpBackend = $httpBackend;
+        expect(service).toBeDefined();
     }));
 
-    it('should do some initial checks', function () {
-        expect(service).toBeDefined();
+    /**
+     * Expect call to API 'api/recipes' and check promise with data
+     */
+    it('should load all recipes', function () {
+        var expectedJson = [{userName: "user1"}, {userName: "user2"}];
 
-        service.addListener();
-
-        service.loadRecipes();
+        var checkResult = function (data) {
+            expect(data).toBeDefined();
+            expect(data[0].userName).toBe("user1");
+            expect(data[1].userName).toBe("user2");
+        };
+        httpBackend.whenGET('/api/recipes').respond(200, expectedJson);
+        service.loadRecipes().then(checkResult);
+        httpBackend.flush();
 
     });
+
+    it('should load recipe by user', function () {
+        var expectedJson = [{userName: "user1", title: "Rezept1"}, {userName: "user1", title: "Rezept2"}];
+
+        var checkResult = function (data) {
+            expect(data).toBeDefined();
+            expect(data[0].userName).toBe("user1");
+            expect(data[0].title).toBe("Rezept1");
+            expect(data[1].userName).toBe("user1");
+            expect(data[1].title).toBe("Rezept2");
+        };
+
+        httpBackend.whenGET('/api/recipes/byUser/123').respond(200, expectedJson);
+        service.loadRecipesByUser("123").then(checkResult);
+        httpBackend.flush();
+
+     // todo   check updateCacheForRecipes
+    });
+
 });
