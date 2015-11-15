9b09445b690aa927ea71f13333c4f7fa18eec5ab
Martin Brändli <martin.braendli@zkb.ch>
2015-11-15 09:37:57 +0100

    removed todo

--- Server/public/js/data/Rating.ts
+++ Server/public/js/data/Rating.ts
@@ -6,7 +6,7 @@ module fettyBossy.Data {
         _id: string;
         recipeId: string;
         userId: string;
-        userName: string; // TODO lookup in user.db instead of saving it into rating.db
+        userName: string;
 
         comment: string;
         stars: number; // 1 to 5 stars
